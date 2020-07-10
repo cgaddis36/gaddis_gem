a21283833354cffa10298bfc4f0f5d98c641c819
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-10 14:37:23 -0600

    initial commit

--- lib/gaddis2020.rb
+++ lib/gaddis2020.rb
@@ -2,5 +2,10 @@ require "gaddis2020/version"
 
 module Gaddis2020
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Fish
+    def fishing_spots
+      puts "This spot is hot!"
+    end
+  end
 end
